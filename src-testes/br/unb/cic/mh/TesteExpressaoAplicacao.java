a40ac547d7be19678c302ea006ca31fb744c29e7
Roberta Renally da Silva Melo <robertarenallydsm@gmail.com>
2016-07-06 18:25:24 -0300

    Testes de Aplicação de Função Concluídos

--- src-testes/br/unb/cic/mh/TesteExpressaoAplicacao.java
+++ src-testes/br/unb/cic/mh/TesteExpressaoAplicacao.java
@@ -21,4 +21,43 @@ public class TesteExpressaoAplicacao extends TesteUtil {
 		
 		Assert.assertEquals(new ValorInteiro(6), ap.avaliar());
 	}
+	@Test
+	public void testeFuncaoDecrementa() {
+		DeclFuncao dec = (new DeclFuncao()).nome("dec")
+				.argumento(new ArgumentoFormal("x", Tipo.INTEIRO))
+				.corpo(subtracao(ref("x"), vi(1)));
+		
+		AmbienteExecucao.instance().declararFuncao(dec);
+		
+		ExpressaoAplicacao ap = (new ExpressaoAplicacao())
+				.nome("dec").parametro(subtracao(vi(3), vi(2)));
+		
+		Assert.assertEquals(new ValorInteiro(0), ap.avaliar());
+	}
+	@Test
+	public void testeFuncaoMultiplicacao() {
+		DeclFuncao mult = (new DeclFuncao()).nome("mult")
+				.argumento(new ArgumentoFormal("x", Tipo.INTEIRO))
+				.corpo(multiplicacao(ref("x"), vi(2)));
+		
+		AmbienteExecucao.instance().declararFuncao(mult);
+		
+		ExpressaoAplicacao ap = (new ExpressaoAplicacao())
+				.nome("mult").parametro(multiplicacao(vi(3), vi(3)));
+		
+		Assert.assertEquals(new ValorInteiro(18), ap.avaliar());
+	}
+	@Test
+	public void testeFuncaoDivisao() {
+		DeclFuncao div = (new DeclFuncao()).nome("div")
+				.argumento(new ArgumentoFormal("x", Tipo.INTEIRO))
+				.corpo(divisao(ref("x"), vi(3)));
+		
+		AmbienteExecucao.instance().declararFuncao(div);
+		
+		ExpressaoAplicacao ap = (new ExpressaoAplicacao())
+				.nome("div").parametro(divisao(vi(81), vi(3)));
+		
+		Assert.assertEquals(new ValorInteiro(9), ap.avaliar());
+	}
 }
--- src-testes/br/unb/cic/mh/TesteUtil.java
+++ src-testes/br/unb/cic/mh/TesteUtil.java
@@ -6,6 +6,18 @@ public abstract class TesteUtil {
 		return new ExpressaoSoma(exp1, exp2);
 	}
 	
+	public ExpressaoSubtrai subtracao(Expressao exp1, Expressao exp2) {
+		return new ExpressaoSubtrai(exp1, exp2);
+	}
+	
+	public ExpressaoMultiplica multiplicacao(Expressao exp1, Expressao exp2) {
+		return new ExpressaoMultiplica(exp1, exp2);
+	}
+	
+	public ExpressaoDivide divisao(Expressao exp1, Expressao exp2) {
+		return new ExpressaoDivide(exp1, exp2);
+	}
+	
 	public ExpressaoRefId ref(String id) {
 		return new ExpressaoRefId(id);
 	}
